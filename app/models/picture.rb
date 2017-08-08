08fe11faa51be893cd67843c248de0a76f56a9fe
Rio3 <r.nyan787@gmail.com>
2017-08-08 17:02:09 +0900

    toriaezu

---
+++ app/models/picture.rb
@@ -0,0 +1,7 @@
+class Picture < ActiveRecord::Base
+
+  has_attached_file :image
+
+  validates_attachment :image, content_type: {content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+end
