1826b8da9c5d96937aa762c8e8de3f436a52bdec
Rio3 <r.nyan787@gmail.com>
2017-08-17 18:46:28 +0900

    [add]0817_massage

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,9 @@
 class Picture < ActiveRecord::Base
 
   has_attached_file :image
-
   validates_attachment :image, content_type: {content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+  validates :name, presence: true
+  validates :image, presence: true
 
 end
